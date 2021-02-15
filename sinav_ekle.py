a9da6c65ff3a18c983435b7c88583a297be97538
tarik1218 <76568517+tarik1218@users.noreply.github.com>
2021-02-15 23:34:48 +0300

    l

--- sinav_ekle.py
+++ sinav_ekle.py
@@ -1,5 +1,5 @@
 from main import *
 #sinav_ekle("fd","di","dl","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe")
 tablo_sil()
-#sinavlar_tablosu_olustur()
+sinavlar_tablosu_olustur()
 #sinav_ekle("fş","123456","dl","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe")
