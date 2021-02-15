db3e1b06f12e0e4e0a547f36f2b22d49470410a0
tarik1218 <76568517+tarik1218@users.noreply.github.com>
2021-02-15 22:52:19 +0300

    w
    w

--- sinav_ekle.py
+++ sinav_ekle.py
@@ -2,3 +2,4 @@ from main import *
 #sinav_ekle("fd","di","dl","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe")
 tablo_sil()
 sinavlar_tablosu_olustur()
+sinav_ekle("fd","di","dl","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe","fe")
