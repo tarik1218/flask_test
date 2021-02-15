68f2ce73ef773420656497f9d1fbce8a3ff90b93
tarik1218 <76568517+tarik1218@users.noreply.github.com>
2021-02-15 10:09:30 +0300

    sd
    ds

--- databasem.py
+++ databasem.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-db = psycopg2.connect(host = "finaldata.postgres.database.azure.com",dbname = "postgres",user = "TarikId1218",password = "Sanane1218*", sslmode="disable" )
+db = psycopg2.connect(host = "finaldata.postgres.database.azure.com",dbname = "postgres",user = "TarikId1218",password = "Sanane1218*", sslmode="require" )
 
 imlec = db.cursor()
 psycopg2.connect()
